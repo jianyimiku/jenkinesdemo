39a979d122766dc32ea286f062f4106764982829
石佳琪 <rt006814@rotai.com>
2021-09-20 10:19:20 +0800

    初始项目构建

---
+++ src/test/java/com/jenkines/jenkinesdemo/JenkinesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkines.jenkinesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
